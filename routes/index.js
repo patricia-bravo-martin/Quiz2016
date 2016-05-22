cafa6a292dc91ba80cf11423d8c1d302b81e70ec
patricia-bravo-martin <p.bravo@alumnos.upm.es>
2016-05-22 20:44:14 +0200

    Página author

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET author page. */
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
+
 module.exports = router;
