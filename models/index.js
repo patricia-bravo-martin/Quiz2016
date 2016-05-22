05fd51b3734c1d79b83b2e554e4eed21f92b50a1
patricia-bravo-martin <p.bravo@alumnos.upm.es>
2016-05-22 23:56:45 +0200

    Despliegue DB en Heroku

--- models/index.js
+++ models/index.js
@@ -3,20 +3,22 @@ var path = require('path');
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite:
+// Usar BBDD Postgres
+
 var url, storage;
+
 if(!process.env.DATABASE_URL){
-url="sqlite:///";
-storage="quiz.sqlite";
+	url="sqlite:///";
+	storage="quiz.sqlite";
 }
 else {
-url=process.env.DATABASE_URL;
-storage=process.env.DATABASE_STORAGE || "";
+	url=process.env.DATABASE_URL;
+	storage=process.env.DATABASE_STORAGE || "";
 }
 
 var sequelize= new Sequelize(url, {
-storage :storage,
-omitNull :true
+							storage :storage,
+							omitNull :true
 });
 
 // Importar la definicion de la tabla Quiz de quiz.js
